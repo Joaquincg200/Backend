999eb8763a9ab24a0d62cc77a5a86a81d28609a9
joaquin <joaquincaravacagarcia200@gmail.com>
2025-12-14 21:05:28 +0100

    Cors3.0

--- src/main/java/com/example/CreArte/security/CorsConfig.java
+++ src/main/java/com/example/CreArte/security/CorsConfig.java
@@ -5,23 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "https://crearte-production.up.railway.app") // tu frontend real
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("https://crearte.up.railway.app") // tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
 }
-
